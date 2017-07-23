712be378f6019983814846984ee49556671f9ea0
rogatec <rogaatec@icloud.com>
2017-07-23 15:24:22 +0200

    refactored with reshaper additionally switched from Assert.AreEqual to Assert.That

---
+++ PageLayoutService/ViewModels/Commands/TransformCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace PageLayoutService.ViewModels.Commands {
+    public sealed class TransformCommand<T> : ICommand {
+        #region "properties"
+
+        private readonly Action<T> _executeMethod;
+        private readonly Predicate<T> _canExecuteEvaluator;
+
+        #endregion "properties"
+
+        #region "constructors"
+
+        public TransformCommand(Action<T> executeMethod, Predicate<T> canExecuteEvaluator = null) {
+            _executeMethod = executeMethod ??
+                             throw new ArgumentNullException(nameof(executeMethod),
+                                 @"Delegate commands can not be null");
+            _canExecuteEvaluator = canExecuteEvaluator;
+        }
+
+        #endregion "constructors"
+
+        #region "methods/events"
+
+#pragma warning disable 0067
+
+        public event EventHandler CanExecuteChanged = delegate { };
+
+#pragma warning restore 0067
+
+        public bool CanExecute(object parameter) {
+            return _canExecuteEvaluator == null || _canExecuteEvaluator((T) parameter);
+        }
+
+        public void Execute(object parameter) {
+            _executeMethod((T) parameter);
+        }
+
+        #endregion "methods/events"
+    }
+}
---
+++ PageLayoutServiceTests/ViewModels/Commands/TransformCommandTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using PageLayoutService.ViewModels;
+
+namespace PageLayoutServiceTests.ViewModels.Commands {
+    [TestFixture()]
+    public class TransformCommandTests {
+        private MainViewModel _viewModel;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp() {
+            _viewModel = new MainViewModel();
+        }
+
+        [Test()]
+        public void Execute() {
+            _viewModel.Transform.Execute(null);
+        }
+
+        [Test()]
+        public void CanExecute() {
+            Assert.That(_viewModel.Transform.CanExecute(null), Is.EqualTo(true));
+        }
+    }
+}
